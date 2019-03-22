b9a12b1abdea89ff3ffa2b7baca60c7eaf55c278
Phillip Koops <peileppe@users.noreply.github.com>
2019-03-22 18:22:56 +0100

    --

--- cobe-journal.py
+++ cobe-journal.py
@@ -4,27 +4,27 @@
 from cobe.brain import Brain
 import readline
 
-# main loop 
+# main loop
 def main():
     readline.parse_and_bind("tab: complete")
     readline.parse_and_bind("set editing-mode vi")
     f= open("journal.txt","a")
     b = Brain("cobe.brain")
     while True:
-	try:
-	    question_Asked = raw_input("User> ").lower()
-	except EOFError:
-	    print
-	    break
-        if question_Asked in ("#quit", "#bye","#exit"):
-          break
-        else:
-	  msg = b.reply(question_Asked)
-	  print "AI> "+msg
-	  b.learn(question_Asked)
-	  f.write("User> "+question_Asked+"\n")
-	  f.write("AI> "+msg+"\n")
-    f.close()
+        try:
+            question_Asked = raw_input("User> ").lower()
+        except EOFError:
+            print
+            break
+            if question_Asked in ("#quit", "#bye","#exit"):
+                break
+            else:
+                msg = b.reply(question_Asked)
+                print ("AI> "+msg)
+                b.learn(question_Asked)
+                f.write("User> "+question_Asked+"\n")
+                f.write("AI> "+msg+"\n")
+        f.close()
     return
-    
-if __name__ == '__main__': main() 
+
+if __name__ == '__main__': main()
